28f9c231b3e96fc8d34988af1c4196a52b3a7c19
Dwarfus <pawasaki@seznam.cz>
2016-10-19 12:30:26 +0100

    Update Generator.py
    Distance calculator works

--- Generator.py
+++ Generator.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-
+from scipy.spatial import distance
 
 class DGenerator():
     def __init__(self, dimension, npoints, mean, variance):
@@ -11,23 +10,40 @@ class DGenerator():
         self.variance = variance        
         self.results = np.array([[0.0]*self.dim]*self.npoints)
         self.generate()
+        self.distance()
         self.plot()
+        
         
     def generate(self):
         i=0
         while (i < self.npoints):
             j=0
-            while (j < self.dim):
-                
+            while (j < self.dim):                
                 self.results[i,j]=np.random.normal(self.mean, self.variance)
                 j+=1       
             i+=1
-        print self.results
+        print(self.results)
         
+    def distance(self):
+        i=0
+        self.minimums=np.array([0.0]*self.npoints)
+        for point in self.results:
+            j=0
+            dist=[]
+            for other in self.results:
+                if (i==j):
+                    None
+                else:
+                   dist.append(distance.euclidean(point, other)) 
+                j+=1
+            self.minimums[i]=min(dist)
+            
+            i+=1
+        print self.minimums
     def plot(self):
         k=0
         x=[]
-        print(self.npoints)
+        
         while(k<self.npoints):
             x.append(self.results[k,0])
             k+=1
@@ -58,6 +74,4 @@ class DGenerator():
         plt.ylabel("ycoordinate")
         plt.grid()
         plt.show()
-
-
         
