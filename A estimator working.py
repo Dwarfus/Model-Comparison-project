465df9b57608fd3948c61e96ab5e55d79a7a9e71
Pavel Kroupa <pawasaki@seznam.cz>
2016-12-06 14:47:57 +0000

    Update and rename +-Fixed A estimator.py to A estimator working.py
    This is the working version of the A estimator for variable L. Version produced on 6.12.2016 14:47 
    Pavel

--- A estimator working.py
+++ A estimator working.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 from scipy.special import binom, gammaln
 from scipy.optimize import minimize_scalar
 """
-THIS WORKS! NEVER ALTER THIS ONE. wORKS FOR 2D BUT VERY NICELY FOR LARGE KTH
+THIS WORKS! NEVER ALTER THIS ONE. Works within a reliable accuracy for variable L
 """
 
 
@@ -123,9 +123,7 @@ class Main:
         
         self.gaussdata,pcov = curve_fit(Main.gauss,self.bincentres,self.histogram[0],p0=[1,np.mean(self.bincentres),1])
         plt.plot(self.bincentres,Main.gauss(self.bincentres,*self.gaussdata),'ro:',label='fit')
-       # print("the fitted variables are: normalisation constant, mean, standard dev")
-       # print(self.gaussdata)        
-        
+  
 
     def analytic(self, r, n):
         """
@@ -135,13 +133,7 @@ class Main:
         dkpdf = (np.exp((self.kth-1)*np.log(lamda)-lamda+np.log(2*np.pi*r*n)-gammaln(self.kth)))
         return dkpdf
 
-    def analytic2(self,r):
-        """Analytic function as from the paper"""
-        self.V0=np.pi**(self.dim/2)/(math.gamma(self.dim/2+1))
-        self.binomial = (binom(self.maindensity-1, self.kth-1))*(self.maindensity-self.kth)
-        anpdf=self.binomial*self.V0**(self.kth)*(1-self.V0*r**self.dim)**(self.maindensity-self.kth-1)*self.dim*r**(self.dim*self.kth-1)
-        
-        return anpdf        
+       
         
     def analyticfit(self):
         """
@@ -153,185 +145,111 @@ class Main:
         
         self.gaussanalyt,pcov2 =curve_fit(Main.gauss,self.steps,self.analytic(self.steps, self.maindensity),p0=[1,np.mean(self.steps),1])
         plt.plot(self.steps,Main.gauss(self.steps,*self.gaussanalyt),'ro:',label='fit')
-        #print("the fitted variables of analytic function are: normalisation constant, mean, standard dev")
-        #print(self.gaussanalyt)
-               
-       # plt.show()
+        plt.show()
         
  
         
     def scale(self):
         """
-        This method takes p(D|n) scales it for every point and its local density ni to p(D|ni)
-        Then it takes this prob and using Bayes theorem scales it into p(n|Di).
-        PDF for each point is then evaluated on range and the gaussians are multiplied (sum of logs). 
-        This leads to a peak which represents the A estimate
+        This method does the most maths. It produces the P(D|ni) for each local density from the original analytic function.
+        This is possible as the original function is a function of n. It was checked to give the same results as the method od scaling one general gaussians parameters.
+        
+        The next step is to change into P(n|D) which is done by rescaling the x axis to find the corresponding n for D. 
+        
+        Next the x-axis is divided by likelihood so we get P(A|D) however likelihood is different for each point.
+        THis means that the values of probabilities no longer correspond to each other. We need to find the parameteres of the gaussian. That is the next part.
+        Here it is extremely important to carefully select the initial guesses for the fitting parameters. However with the use of the P(D|n0) parameters we can do it easily.
+        
+        With the parameters we can evaluate them at some universal points(same for all gaussians) and do the sum of logs. Getting a peaked value
         """
-        self.localgauss=np.zeros((len(self.density),3))
-        i=0
-        for value in self.density:
-            scale = (self.maindensity/value)**(1/self.dim)
-            self.localgauss[i][0]= self.gaussanalyt[0]/scale
-            self.localgauss[i][1]=self.gaussanalyt[1]*scale
-            self.localgauss[i][2] = self.gaussanalyt[2]*scale
-            
-               
-            i+=1
-        #print(self.localgauss) 
-        self.steps2=np.linspace(0.1,self.gaussanalyt[1]*3,400)
+
+        # steps2 are for evaluation of pdf based on local density
+        self.steps2=np.linspace(self.minbin/3,self.gaussanalyt[1]*3,400)
         self.probabilities=np.zeros((len(self.density), len(self.steps2)))
         j=0
-        plt.figure(3)
-        for gauss in self.localgauss:
-            self.probabilities[j]=Main.gauss(self.steps2,*gauss)
+        plt.figure(2)
+        for value in self.density:
+            self.probabilities[j]=self.analytic(self.steps2, value)
             plt.plot(self.steps2, self.probabilities[j])
             
-            plt.title("scale fit")
+            plt.title("Pdf of P(D|n)")
             j+=1
         plt.show()    
+        
+        
+        
+        # Now I need to change into P(n|D)
         self.newsteps=self.kth/(math.pi*(self.steps2)**2)
         
-        ##########################################################
+        # Now for each point the x-axis is divided by the likelihood. The probabilities no longer correspond to the same points
+        # So we need to find the parameters of the gaussians to then evaluate all of them at the same points
         self.parameters=np.zeros((self.npoints,3))
         m=0
-        plt.figure(10)
-        plt.title("scale fit scaled by L")
-        #plt.plot(self.newsteps, self.probabilities[0])
-        #plt.show()
-        #print(len(self.probabilities[0]), len(self.newsteps))
-        #print(self.probabilities[0])
-        #print(self.like)
+
+        
+        guess = self.kth/(math.pi*self.gaussanalyt[1]**2) # estimate the mean of the densities of the gaussians to use as initial guess after dividing by likelihood
+        totalguess=0
         for value in self.like:
 
             newsteps = self.newsteps/value
-            #print
- 
-            self.para, pcov2 =curve_fit(Main.gauss,newsteps,self.probabilities[m],p0=[10,1500,400])
-            plt.plot(newsteps, self.probabilities[m])
-            #print("hi",self.para)
-            self.parameters[m]=self.para
-            #print("hi",self.parameters[m],m)
-            
-            
+            totalguess=totalguess+guess/value    
+            self.para, pcov2 =curve_fit(Main.gauss,newsteps,self.probabilities[m],p0=[self.gaussanalyt[0],guess/value,guess/(value*4)])
+            self.parameters[m]=self.para            
             m+=1
-        #print(self.parameters)
-        plt.show() 
-        
-        self.steps3 = np.linspace(1,5000,200)
-        
-        
-        self.logsum2=np.zeros(( len(self.steps3)))
-        plt.show()
-       # print(self.logsum.shape)
+        totalguess=totalguess/m # Total guess is an average of the means of all the gaussians of P(A|D) serves as a mid point for the fitting so no important values are omitted.
+ 
+        # FRom parameters we evaluate the gaussians and add the log
+        self.steps3 = np.linspace(1,totalguess*4,200) # these steps are used for plotting for logsums
+
+        self.logsum=np.zeros(( len(self.steps3)))
         
         j=0
+        plt.figure(4)
+        plt.title("P(A|Di)")
         self.probabilities2=np.zeros((len(self.density), len(self.steps3)))
         for value2 in self.parameters:
-            #print("hi",value)
+            
             self.probabilities2[j]=Main.gauss(self.steps3,*value2)
             plt.plot(self.steps3, self.probabilities2[j])
-            
-            plt.title("scale fit2222")
+                        
             j+=1
         plt.show()
         
-       # print(self.probabilities2[1])
-        #self.probabilities2
+        # Here we add the logsums
         k=0
         for prob2 in self.probabilities2:
-            #print("hi",prob)
-            prob2[prob2<=0]=1
+            prob2[prob2<=0]=1 # for any forbiden value (zero or negative probability) we assign 1 which contributes nothing to the log sum. 
             
-           # print(self.logsum2[50], prob2[50])
-            self.logsum2=np.add(self.logsum2,np.log(abs(prob2)))
+            self.logsum=np.add(self.logsum,np.log(abs(prob2)))
             
             k+=1
-       
-        #print(self.logsum2)
-
 
         plt.figure(11)
-        plt.title("Peak 222222")
-        plt.plot(self.steps3,np.exp(self.logsum2))
-        plt.show()
-        self.max_x2 = self.steps3[self.logsum2.argmax()]
-        self.max_y2 = max(self.logsum2)
-        print("The estimate of A of gauss",self.max_x2, self.max_y2)
-        
-        
-        
-        
-        
-        
-        
-       
-       ########################################################################  
-       
-       
-       
-        self.logsum=np.zeros(( len(self.steps2)))
-        plt.show()
-       # print(self.logsum.shape)
-        k=0
-        
-        for prob in self.probabilities:
-            #print(self.logsum[80], prob[80])
-            self.logsum=np.add(self.logsum,np.log(prob))
-            
-            k+=1
-       
-        #print(self.logsum)
-        self.newsteps=self.newsteps/self.like[1]
-
-        plt.figure(5)
         plt.title("Peak")
-        plt.plot(self.newsteps,np.exp(self.logsum))
+        plt.plot(self.steps3,np.exp(self.logsum))
         plt.show()
-        self.max_x = self.newsteps[self.logsum.argmax()]
+        self.max_x = self.steps3[self.logsum.argmax()]
         self.max_y = max(self.logsum)
-        print("The estimate of A",self.max_x)
-
+        print("The estimate of A and the 'probability' value at that point is",self.max_x, self.max_y)
         
-        #other, self.nnewsteps=np.split(self.newsteps,[(self.logsum.argmax())*0.5])
-       # other2, self.nlogsum=np.split(self.logsum,[(self.logsum.argmax())*0.5])
-
-       # plt.figure(6)
-       # plt.title("Peak2")
-       # plt.plot(self.nnewsteps,np.exp(self.nlogsum))
-        #plt.show()
-        
-    def parameterA(self):
-        means = []
-        sigmas = []
-        for parameter in self.parameters:
-            means.append(parameter[1])
-            sigmas.append(parameter[2])
-        invvars = []
-        scaledmeans = []
-        i = 0
-        for sigma in sigmas:
-            invvars.append(1 / sigma ** 2)
-            scaledmeans.append(means[i]/ sigma ** 2)
-            i += 1
-        totalvar = 1/ sum(invvars)
-        self.parameterA = sum(scaledmeans) * totalvar
-        print(self.parameterA, totalvar)
+           
+      
         
 
     def call(self):
-        #self.gaussian()
+
         self.flat()
         self.distance()
         self.density()
         self.plot()
         self.fit()
         self.analyticfit()
-        #self.aestimator()
         self.scale()
-        self.parameterA()
+
        
        
         """Comment> The flattening of the D distribution might be due to boundary effects. which makes the distance larger than it should actually be"""
 
         
-a=Main(2,1500,[1.0,1.0],25)            
+a=Main(2,650,[1.0,1.0],20)            
+
