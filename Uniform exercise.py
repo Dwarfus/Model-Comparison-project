67d8f63db40df52424f0880f1aa77f363158156d
Pavel Kroupa <pawasaki@seznam.cz>
2017-01-21 11:19:11 +0000

    Final version 2D uniform distribution exercise

--- Uniform exercise.py
+++ Uniform exercise.py
@@ -16,43 +16,29 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 from scipy.special import binom
 from scipy.optimize import curve_fit, minimize_scalar
-"""
-THIS WORKS! NEVER ALTER THIS ONE. wORKS FOR 2D BUT VERY NICELY FOR LARGE KTH
-"""
+
 
 
 class Main:
     def __init__(self, dim, npoints, mean=[0.0,0.0], kth=1 ):
+        """
+        Initiates the necessary values as given by the user
+        """
         self.data=np.array([[0.0]*dim]*npoints)
         self.dim=dim
         self.npoints=npoints
         self.mean=mean
         self.kth=kth
-        self.like=np.array([0.0]*self.npoints) # Here the likelihood of each point will be stored
+        self.like=np.array([0.0]*self.npoints) 
+        # Here the likelihood of each point will be stored
         
         self.call()
         
-   
-    def gaussian(self):
-        """
-        The generator of data with gaussian distribution of points
-        """
-        i=0
-        variance=1
-        while (i < self.npoints):
-          
-                            
-            self.data[i] = np.random.normal(self.mean, variance)
-            self.like[i] = 1/(2*np.pi)*np.exp(-(self.data[i,0]-self.mean[0])**2/2*variance**2)*np.exp(-(self.data[i,1]-self.mean[1])**2/2*variance**2)
-            i+=1
-        self.volume = math.pi**(self.dim/2)*(variance*2)**(self.dim)/math.gamma(self.dim/2+1)    # the factor around variance is the number of sigma
-        self.maindensity=self.npoints/self.volume    
-        print("main density is", self.maindensity)
 
         
     def flat(self):
         """
-        This method generate data according to a flat distribution
+        This method generate data according to a uniform distribution
         """
         i=0
         self.x=4
@@ -70,7 +56,8 @@ class Main:
     
     def distance(self):
         """
-        This method calculates the kth nearest neighbour distance. The results are saved in 1D array
+        This method calculates the kth nearest neighbour distance. 
+        The results are saved in 1D array
         """
         i=0
         self.distance=np.array([0.0]*self.npoints)
@@ -93,13 +80,15 @@ class Main:
         self.density=np.array([0.0]*self.npoints)
         i=0
         while i<len(self.distance):
-            self.density[i] = self.kth*math.gamma(self.dim/2+1)/(math.pi**(self.dim/2)*self.distance[i]**self.dim)
+            self.density[i] =self.kth*math.gamma(self.dim/2+1) \
+                            /(math.pi**(self.dim/2)*self.distance[i] \
+                              **self.dim)
             i+=1  
             
             
     def plot(self):
         """
-        This method plots the histogram of the kth nearest neighbout distances
+        This method plots the histogram of the kth nearest neighbout distance.
         """
         plt.figure(1)   
         self.histogram = plt.hist(self.distance,bins="auto", normed=True )       
@@ -114,7 +103,8 @@ class Main:
         return a*math.e**(-(x-x0)**2/(2*sigma**2))
         
     def fit(self):
-        """This methods fits the histogram with gaussian curve and finds the parameters of the curve."""               
+        """This methods fits the histogram with gaussian curve and finds the parameters of the curve. 
+        This values is a check that the analytic function is simillar to this one"""               
         j=0
         self.bincentres=np.array([0.0]*(len(self.histogram[1])-1))        
         while j<(len(self.histogram[1])-1):        
@@ -123,19 +113,17 @@ class Main:
         
         self.gaussdata,pcov = curve_fit(Main.gauss,self.bincentres,self.histogram[0],p0=[1,np.mean(self.bincentres),1])
         plt.plot(self.bincentres,Main.gauss(self.bincentres,*self.gaussdata),'ro:',label='fit')
-        print("the fitted variables are: normalisation constant, mean, standard dev")
-        print(self.gaussdata)        
-        
+
 
     def analytic(self, r, n):
         """
-        Analytic function obtained through Poisson method"""
+        Analytic function obtained through Poisson method for p(D|n)"""
         lamda = np.pi**(self.dim/2)*r**(self.dim)/math.gamma(self.dim/2+1)*n
         dkpdf= (lamda**(self.kth-1)*np.exp(-lamda)*2*np.pi*r*n/math.factorial(self.kth-1))
         return dkpdf
 
     def analytic2(self,r):
-        """Analytic function as from the paper"""
+        """Analytic function as from the paper. Gives same answer as the above"""
         self.V0=np.pi**(self.dim/2)/(math.gamma(self.dim/2+1))
         self.binomial = (binom(self.maindensity-1, self.kth-1))*(self.maindensity-self.kth)
         anpdf=self.binomial*self.V0**(self.kth)*(1-self.V0*r**self.dim)**(self.maindensity-self.kth-1)*self.dim*r**(self.dim*self.kth-1)
@@ -144,6 +132,7 @@ class Main:
     def analyticfit(self):
         """
         Fits the analytic function with a gaussian to get the parameters
+        Used for initial estimates and x-axis interval choice.
         """
         self.maxbin=max(self.histogram[1])
         self.minbin=min(self.histogram[1])
@@ -151,18 +140,14 @@ class Main:
         
         self.gaussanalyt,pcov2 =curve_fit(Main.gauss,self.steps,self.analytic(self.steps, self.maindensity),p0=[1,np.mean(self.steps),1])
         plt.plot(self.steps,Main.gauss(self.steps,*self.gaussanalyt),'ro:',label='fit')
-        print("the fitted variables of analytic function are: normalisation constant, mean, standard dev")
-        print(self.gaussanalyt)
-               
-        plt.show()
-        
+
  
 
     def aestimator(self):
         """
-        This method estimates the A based on the following approach: From nearest neighbour distances the local density can be estimated. 
-        From this the A can be estimated - the histogram will have A on the x axis. The mean of fitted gaussian can be considered as the A estimate??
-        This is one way I understood the method 1 given last week. 
+        This is an alternative method to evaluate A. For each point the most probable n is evaluated from its D.
+        From n, A was calculated. These values of A were plotted as a histogram.
+        The histogram has a Gaussian shape, which mean was taken as a A estimate. Works well for uniform likelihood.
         """
         self.a=self.density/self.like
         
@@ -178,17 +163,14 @@ class Main:
         maxhist = max(self.histogram2[0])
         for hbin in self.histogram2[0]:
             if( hbin== maxhist):
-                #print("hey")
                 maxvalue=i
                 None
             i+=1
             
                         
         j=0
-        self.bincentres2=np.array([0.0]*(len(self.histogram2[1])-1))
-        
-        while j<(len(self.histogram2[1])-1):
-            
+        self.bincentres2=np.array([0.0]*(len(self.histogram2[1])-1))        
+        while j<(len(self.histogram2[1])-1):            
             self.bincentres2[j]=(self.histogram2[1][j]+self.histogram2[1][j+1])/2           
             j+=1
 
@@ -202,8 +184,9 @@ class Main:
     def scale(self):
         """
         This method takes p(D|n) scales it for every point and its local density ni to p(D|ni)
-        Then it takes this prob and using Bayes theorem scales it into p(n|Di).
-        PDF for each point is then evaluated on range and the gaussians are multiplied (sum of logs). 
+        Then it takes this prob and by recalculating D into n evaluates p(n).
+        The n values are then divided by L to give p(A)
+        The gaussians are multiplied (sum of logs). 
         This leads to a peak which represents the A estimate
         """
         self.localgauss=np.zeros((len(self.density),3))
@@ -216,52 +199,52 @@ class Main:
             i+=1
             
 
-        self.steps2=np.linspace(0.005,self.gaussanalyt[1]*4,500)
+        self.steps2=np.linspace(0.03,self.gaussanalyt[1]*4,500) # D values
         self.probabilities=np.zeros((len(self.density), len(self.steps2)))
         j=0
         plt.figure(3)
+        plt.title("p(D|n)")
+        plt.xlabel("n")
+        plt.ylabel("Relative Probability")
         for gauss in self.localgauss:
             self.probabilities[j]=Main.gauss(self.steps2,*gauss)
-            plt.plot(self.steps2, self.probabilities[j])
-            
-            plt.title("scale fit")
+            plt.plot(self.steps2, self.probabilities[j])                        
             j+=1
 
-        self.newsteps=self.kth/(math.pi*(self.steps2)**2)
-
+        self.newsteps=self.kth/(math.pi*(self.steps2)**2) # n values
         
         self.logsum=np.zeros(( len(self.steps2)))
-        print(self.logsum.shape)
-        k=0
-        
-        for prob in self.probabilities:
- 
+
+        k=0        
+        for prob in self.probabilities: 
             self.logsum=np.add(self.logsum,np.log(prob))
             k+=1
        
 
-        self.newsteps=self.newsteps/self.like[1]
+        self.newsteps=self.newsteps/self.like[1] #A values
+
+        plt.figure(4)
+        plt.title("p(A|D)")
+        plt.xlabel("A value")
+        plt.ylabel("Relative Probability")
+        for prob in self.probabilities:
+            plt.plot(self.newsteps, prob)
+            
+        plt.show()
 
         plt.figure(5)
         plt.title("Peak")
+        plt.xlabel("A value")
+        plt.ylabel("Relative Probability")        
         plt.plot(self.newsteps,np.exp(self.logsum))
         plt.show()
+        
         self.max_x = self.newsteps[self.logsum.argmax()]
         self.max_y = max(self.logsum)
         print("The estimate of A",self.max_x)
 
         
-        #other, self.nnewsteps=np.split(self.newsteps,[(self.logsum.argmax())*0.5])
-       # other2, self.nlogsum=np.split(self.logsum,[(self.logsum.argmax())*0.5])
-
-       # plt.figure(6)
-       # plt.title("Peak2")
-       # plt.plot(self.nnewsteps,np.exp(self.nlogsum))
-        #plt.show()
-        
-
     def call(self):
-        #self.gaussian()
         self.flat()
         self.distance()
         self.density()
@@ -271,8 +254,5 @@ class Main:
         self.aestimator()
         self.scale()
        
-       
-        """Comment> The flattening of the D distribution might be due to boundary effects. which makes the distance larger than it should actually be"""
-
-        
-a=Main(2,500,[1.0,1.0],20)            
+                    
+a=Main(2,500,[1.0,1.0],10)       
