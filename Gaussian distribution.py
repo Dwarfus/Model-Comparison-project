ab9f6eed01aceb0084dfadfd8920e0dc4aa9d510
Pavel Kroupa <pawasaki@seznam.cz>
2017-01-21 11:28:45 +0000

    Final version of Gaussian distribution
    This include all the optimising algorithms for high M, N

--- Gaussian distribution.py
+++ Gaussian distribution.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Nov 29 10:55:38 2016
 @author: donukb and Pavel
-This is (hopefully) the (semi)final version of this code.
+
 ~ Only applies to 2D gaussian distribution ~
 """
 import numpy as np
@@ -24,17 +24,15 @@ from scipy import special
 
 class Generate():
     """
-    Generates data from a 2D Gaussian distribution
+    Generates data from a 2D Gaussian distribution with a user-defined mean and standard deviation
     """
     def __init__(self, dimension, npoints, mean = [0.0,0.0], variance = [1.0,1.0]):
         self.dim = dimension
         self.npoints = npoints        
         self.data = np.array([[0.0]*self.dim]*self.npoints)
         self.means = mean
-        self.variances = variance
-        
+        self.variances = variance        
         self.gaussian()
-        #self.writetofile()
         self.testplot()
         
     def gaussian(self):
@@ -46,16 +44,13 @@ class Generate():
                 k+=1       
             j+=1   
     
-            
-    #def writetofile(self):
-        #file = open('GaussianData.txt','w')
-        #file.write(self.data) <= needs to be string argument, change later.
-        #file.close()
         
     def testplot(self):
         self.plotdata = [[a for a,b in self.data],[b for a,b in self.data]]
         plt.figure(1)
         self.testplot = plt.scatter(self.plotdata[0],self.plotdata[1])
+        plt.show()
+        print("data generated")
         
 class NumericalA():
     """
@@ -71,13 +66,11 @@ class NumericalA():
         self.xdata = [a for a,b in self.xydata]
         self.ydata = [b for a,b in self.xydata]
         self.xdatamean = sum(self.xdata) / self.bigM
-        self.ydatamean = sum(self.ydata) / self.bigM
-        
-
+        self.ydatamean = sum(self.ydata) / self.bigM        
         self.call()
         
     def sums(self):
-        """A module to define all the sums needed in the likelihood module"""
+        """A module to define all the sums needed in the likelihood module and the analytic posterior evaluation"""
         i = 0
         xsqr = []
         ysqr = []
@@ -101,19 +94,15 @@ class NumericalA():
         self.yiyjsum = sum(yiyj) / 2
         
     def likelihood(self):
-        """This should two lists of generated points from the likelihood functions in parameter space with a fixed dataset"""
+        """This generates the points in parameter space for a given xmean and ymean and M number of points in data space"""
         self.mu1gauss = np.random.normal(self.xdatamean, 1/np.sqrt(self.bigM), self.npoints)
         self.mu2gauss = np.random.normal(self.ydatamean, 1/np.sqrt(self.bigM), self.npoints)
-        #self.mu1data = np.sqrt(2*np.pi)/self.bigM * 1/((2*np.pi)**(self.bigM/2)) * np.exp(1/(self.bigM ** 2) * self.xsqrsum - self.xixjsum) * self.mu1gauss
-        #self.mu2data = np.sqrt(2*np.pi)/self.bigM * 1/((2*np.pi)**(self.bigM/2)) * np.exp(1/(self.bigM ** 2) * self.ysqrsum - self.yiyjsum) * self.mu2gauss
         i = 0
         self.mudata = []
         while i < self.npoints:
             self.mudata.append([self.mu1gauss[i],self.mu2gauss[i]])
             i += 1
             
-        mu1sorted = sorted(self.mu1gauss)
-        mu2sorted = sorted(self.mu2gauss)
         plt.figure(2)
         self.likeplot = plt.scatter(self.mu1gauss, self.mu2gauss)
         plt.axis([-3/np.sqrt(self.bigM), 3/np.sqrt(self.bigM), -3/np.sqrt(self.bigM), 3/np.sqrt(self.bigM)])
@@ -131,20 +120,12 @@ class NumericalA():
         # took out factor of 1/(2 * np.pi) ** (npoints/2) * 
         
 
-    def pointprob(self,u1,u2,xav,yav,std):
-        probx = NumericalA.gauss(u1,1/(np.sqrt(2*math.pi)*std),xav,std)
-        proby = NumericalA.gauss(u2,1/(np.sqrt(2*math.pi)*std),yav,std)
-        return probx*proby
-
     def gauss( x,a,x0,sigma):
         """Prescription for gauss distribution"""
         return a*math.e**(-(x-x0)**2/(2*sigma**2))
         
     def Probability(self):
-        """ p(x) = integral(dmu1 dmu2 L(mu1, mu2) * uniform prior)"""
-        #self.testfunc = lambda x: x**2
-        #self.testintegral = integrate.quad(self.testfunc,1,2)
-        #print("testfunc", self.testintegral)
+        """ p(x) = integral(dmu1 dmu2 L(mu1, mu2) * uniform prior) This evaluate analyticaly the posterior*prior"""
         self.mu1likefunc = lambda x: NumericalA.likefunc1D(x,self.bigM,self.xsqrsum,self.xixjsum,self.xdatamean)
         self.mu1probability = integrate.quad(self.mu1likefunc,-np.inf,np.inf)
         self.mu2likefunc = lambda y: NumericalA.likefunc1D(y,self.bigM,self.ysqrsum,self.yiyjsum,self.ydatamean)
@@ -165,7 +146,6 @@ class NumericalA():
                     distances.append(distance.euclidean(point1,point2))
                     j += 1
             distances.sort()
-            #self.kth = 2
             self.NNdistlist.append(distances[self.kth -1])
             i += 1
             
@@ -181,7 +161,7 @@ class NumericalA():
         self.radius = distancesfromcentre[-1]
 
     def maindensity(self):
-        """Finds the total density of the parameter space data"""
+        """Finds the overall density of the parameter space data"""
         self.volume = math.pi**(self.dim/2)*(self.radius)**(self.dim)/math.gamma(self.dim/2+1)    # the factor around variance is the number of sigma
         self.maindensity=self.npoints/self.volume
         print("Total Parameter Space Density =", self.maindensity)
@@ -200,9 +180,8 @@ class NumericalA():
         self.locallike = []
         for point in self.mudata:
             self.locallike.append(NumericalA.likefunc1D(point[0],self.npoints,self.xsqrsum,self.xixjsum,self.xdatamean) * NumericalA.likefunc1D(point[1],self.npoints,self.ysqrsum,self.yiyjsum,self.ydatamean))
-            #self.locallike.append(self.pointprob(point[0],point[1],self.xdatamean,self.ydatamean,1/np.sqrt(self.bigM)))
+           
  
-
     def plot(self):
         """
         This method plots the histogram of the kth nearest neighbout distances
@@ -213,13 +192,11 @@ class NumericalA():
         plt.title("Nearest Neighbour #%s" %(self.kth))
         plt.xlabel("Distance")
         plt.ylabel("Probability") 
-
         
-
-
         
     def fit(self):
-        """This methods fits the histogram with gaussian curve and finds the parameters of the curve."""               
+        """This methods fits the histogram with gaussian curve and finds the parameters of the curve. 
+        Should be similar to the analytic function below"""               
         j=0
         self.bincentres=np.array([0.0]*(len(self.histogram[1])-1))        
         while j<(len(self.histogram[1])-1):        
@@ -231,9 +208,9 @@ class NumericalA():
         
     def analytic(self, r, n):
         """
-        Analytic function obtained through Poisson method"""
-        lamda = np.pi**(self.dim/2)*r**(self.dim)/math.gamma(self.dim/2+1)*n
-        #dkpdf= (lamda**(self.kth-1)*np.exp(-lamda)*2*np.pi*r*n/math.factorial(self.kth-1))
+        Analytic function obtained through Poisson method. 
+        Gives the p(D|n)"""
+        lamda = np.pi**(self.dim/2)*r**(self.dim)/math.gamma(self.dim/2+1)*n        
         dkpdf = (np.exp((self.kth-1)*np.log(lamda)-lamda+np.log(2*np.pi*r*n)-gammaln(self.kth)))
         return dkpdf
 
@@ -241,74 +218,72 @@ class NumericalA():
         
     def analyticfit(self):
         """
-        Fits the analytic function with a gaussian to get the parameters
+        Fits the analytic function with a gaussian to get the parameters for the overall density.
+        This is used as a guess for picking the steps in next section.
+        Steps= the values of D on which to evaluate the function.
         """
         self.maxbin=max(self.histogram[1])
         self.minbin=min(self.histogram[1])
         self.steps=np.linspace(self.minbin/5, self.maxbin, 500)
-        intmax= np.argmax(self.histogram[0])
-       # print("intmax is", self.histogram[0][intmax],self.histogram[1][intmax])
+        intmax= np.argmax(self.histogram[0])      
         probs = self.analytic(self.steps, self.maindensity)
         self.gaussanalyt,pcov2 =curve_fit(NumericalA.gauss,self.steps,self.analytic(self.steps, self.maindensity),p0=[self.histogram[0][intmax]/3 ,self.histogram[1][intmax],self.histogram[1][intmax]/5])
         plt.plot(self.steps,NumericalA.gauss(self.steps,*self.gaussanalyt),'ro:',label='fit')
         plt.plot(self.steps, probs )
         plt.show()
-        #print(self.gaussanalyt)
+
+
+
  
         
     def scale(self):
         """
         This method does the most maths. It produces the P(D|ni) for each local densitylist from the original analytic function.
-        This is possible as the original function is a function of n. It was checked to give the same results as the method od scaling one general gaussians parameters.
+        This is possible as the original function is a function of n. It was checked to give the same results as the method using the pdf from the paper.
         
         The next step is to change into P(n|D) which is done by rescaling the x axis to find the corresponding n for D. 
         
         Next the x-axis is divided by likelihood so we get P(A|D) however likelihood is different for each point.
-        THis means that the values of probabilities no longer correspond to each other. We need to find the parameteres of the gaussian. That is the next part.
+        This means that the values of probabilities no longer correspond to each other. We need to find the parameteres of the gaussian. That is the next part.
         Here it is extremely important to carefully select the initial guesses for the fitting parameters. However with the use of the P(D|n0) parameters we can do it easily.
         
         With the parameters we can evaluate them at some universal points(same for all gaussians) and do the sum of logs. Getting a peaked value
+        
         """
-
-        # steps2 are for evaluation of pdf based on local density
-        self.steps2=np.linspace(self.minbin/5,self.gaussanalyt[1]*20,20000)
+        
+       
+        self.steps2=np.linspace(self.minbin/6,self.gaussanalyt[1]*40,20000) # steps2 are D values
         self.probabilities=np.zeros((len(self.densitylist), len(self.steps2)))
         j=0
-        plt.figure(2)
+        plt.figure(5)
         for value in self.densitylist:
             self.probabilities[j]=self.analytic(self.steps2, value)
-            plt.plot(self.steps2, self.probabilities[j])
-            
+            plt.plot(self.steps2, self.probabilities[j])            
             plt.title("Pdf of P(D|n)")
             j+=1
         plt.show()    
         
-        count = 0
+        count = 0 # This was a check for negative values as they posed a problem during one stage of the development
         for prob in self.probabilities:
             for number in prob:
                 if number<0:
                     count+=1
-        print(count)
+        #print(count)
         
         # Now I need to change into P(n|D)
-        self.newsteps=self.kth/(math.pi*(self.steps2)**2)
+        self.newsteps=self.kth/(math.pi*(self.steps2)**2) # newsteps are the n values 
+       # print(self.newsteps)
         plt.figure(10)
         for prob in self.probabilities:
             plt.plot(self.newsteps, prob)
-        plt.title("P(n|D)")
-        #plt.xlim([0,5e6])
+        plt.title("P(n)")
         plt.show()
         
         # Now for each point the x-axis is divided by the likelihood. The probabilities no longer correspond to the same points
         # So we need to find the parameters of the gaussians to then evaluate all of them at the same points
         self.parameters=np.zeros((self.npoints,3))
-        m=0
-
-        
-        
-        
-        
-        #guess = self.kth/(math.pi*self.gaussanalyt[1]**2) # estimate the mean of the densities of the gaussians to use as initial guess after dividing by likelihood
+        m=0                
+                
         guess = self.maindensity        
         totalguess=0
     
@@ -317,26 +292,23 @@ class NumericalA():
             inti = self.probabilities[m].argmax()
             const = self.probabilities[m][inti]
             mean = newstepsl[inti]
-            #print(const,mean)
-            self.para, pcov2 =curve_fit(NumericalA.gauss,newstepsl,self.probabilities[m],p0=[const,mean,mean])
-            totalguess=totalguess+mean    
-            #self.para, pcov2 =curve_fit(NumericalA.gauss,newstepsl,self.probabilities[m],p0=[abs(self.gaussanalyt[0]),guess/value,guess/(value*4)])
-            self.parameters[m]=self.para    
-           # if m<50 or m>950:
-           #     print("hi", self.parameters[m], const, mean,"std", np.sqrt(np.diag(pcov2)))
-            m+=1
-        totalguess=totalguess/m # Total guess is an average of the means of all the gaussians of P(A|D) serves as a mid point for the fitting so no important values are omitted.
-        #print(self.locallike)
-        # FRom parameters we evaluate the gaussians and add the log      
-        
-        self.steps3 = np.linspace(1,totalguess*40,5000) # these steps are used for plotting for logsums
 
+            self.para, pcov2 =curve_fit(NumericalA.gauss,newstepsl,self.probabilities[m],p0=[const,mean,mean/4])
+            totalguess=totalguess+mean                
+            self.parameters[m]=self.para              
+            m+=1
+            
+        totalguess=totalguess/m # Total guess is an average of the means of all the gaussians of P(A) serves as a mid point for the fitting so no important values are omitted.
+
+
+        # From parameters we evaluate the gaussians and add the log of them             
+        self.steps3 = np.linspace(1,totalguess*40,5000) # these steps are used for evaluating A
         self.logsum=np.zeros(( len(self.steps3)))
         
         j=0
         plt.figure(4)
         plt.title("P(A|Di)")
-        #plt.xlim([0,200000])
+
         self.probabilities2=np.zeros((len(self.densitylist), len(self.steps3)))
         for value2 in self.parameters:
 
@@ -350,9 +322,8 @@ class NumericalA():
         k=0
         for prob2 in self.probabilities2:
             prob2[prob2<=0]=1 # for any forbiden value (zero or negative probability) we assign 1 which contributes nothing to the log sum. 
-            
-            self.logsum=np.add(self.logsum,np.log(abs(prob2)))
-            
+            # The above was done as for a while negative values were found. However this is no longer the case with better initial guesses for the fits
+            self.logsum=np.add(self.logsum,np.log(abs(prob2)))            
             k+=1
 
         plt.figure(11)
@@ -362,11 +333,16 @@ class NumericalA():
         self.max_x = self.steps3[self.logsum.argmax()]
         self.max_y = max(self.logsum)
         print("The estimate of A and the 'probability' value at that point is",self.max_x, self.max_y)
-  
+        self.parameterA()
 
 
         
     def parameterA(self):
+        """
+        Another way of getting the final estimate A. 
+        Using the fact that the peak is just mean of the overall Gaussian,
+        which in turn is just a product of all the Gaussian distributions.
+        There exist an analytical expression for this and is used below."""
         means = []
         sigmas = []
         for parameter in self.parameters:
@@ -380,8 +356,7 @@ class NumericalA():
             scaledmeans.append(means[i]/ sigma ** 2)
             i += 1
         totalvar = 1/ sum(invvars)
-        self.parameterA = sum(scaledmeans) * totalvar
-        print(self.parameterA, totalvar)  
+        self.paramA = sum(scaledmeans) * totalvar 
         self.parA=self.npoints/(self.mu1probability[0] * self.mu2probability[0])
         print("expected value of A is:", self.parA)
         
@@ -390,34 +365,16 @@ class NumericalA():
         self.likelihood()
         self.Probability()
         self.NNdistance()
+        print("distance done")
         self.findradius()
         self.maindensity()
         self.plot()
-        #self.fit()
         self.localdensity()
         self.locallikelihood()
         self.analyticfit()
+        print("analyticfit done")
         self.scale()
-        self.parameterA()
-
-
         
-"""Below are just some test values to play around with."""
-#c = Generate(2,5000)
-results=[]
 
-b = NumericalA(c,5000,5)
-#results.append([b.max_x,b.max_y, b.parameterA])
-#b = NumericalA(c,1200,10)
-#results.append([b.max_x,b.max_y, b.parameterA])
-#b = NumericalA(c,2000,50)
-#results.append([b.max_x,b.max_y, b.parameterA])
-print(results)
-
-"""
-file = open('GaussianData.txt','w')
-for value in c.data:
-    file.write(str(value))
-    file.write("\n") 
-file.close()
-"""
+c = Generate(2,500)
+b = NumericalA(c,500,5)
